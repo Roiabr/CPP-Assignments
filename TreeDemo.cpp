5da92ecc68b823dbd87552fbffcab1a6554073e0
Roi Abramovitch <44756354+Roiabr@users.noreply.github.com>
2019-03-24 23:30:29 +0200

    first assignemt

---
+++ TreeDemo.cpp
@@ -0,0 +1,45 @@
+/**
+ * Demo program for the exercise on binary trees
+ *
+ * @author Erel Segal-Halevi
+ * 
+ * @since 2019-02
+ */
+
+#include <iostream>
+using std::cout, std::endl;
+
+#include "Tree.hpp"
+
+int main() {
+  try {
+    // constructs an empty tree:
+    ariel::Tree emptytree;
+    cout << "emptytree: size=" << emptytree.size() << endl; // should print 0
+    
+    // constructs an ordered binary tree where:
+      // 5 is in the root;
+      // 3 is the root's left child;
+      // 7 is the root's right child.
+    ariel::Tree threetree;
+    threetree.insert(5);
+    threetree.insert(7);
+    threetree.insert(3);
+    threetree.insert(16);
+    cout << "threetree: size=" << threetree.size() << " root=" << threetree.root() << endl;  // size=3, root=5.
+    threetree.printf();
+    cout << "is the number in the tree? " << threetree.contains(5) << endl;
+    cout <<"the parent is: " << threetree.parent(3);
+    cout << endl;
+      
+    cout << "second size: " << threetree.size()      // should print 3
+         //<< threetree.parent(3)   // should print 5
+         //<< threetree.parent(7)   // should print 5
+         //<< threetree.left(5)     // should print 3
+         //<< threetree.right(5)    // should print 7
+         << endl;
+    threetree.insert(5);    // should throw an exception, since 5 already exists.
+  } catch (...) {
+    cout << "Caught exception!" << endl;
+  }
+}
